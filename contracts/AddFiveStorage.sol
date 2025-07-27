2051efd46b6fc25495be750f7445f86ed2a6ef2f
ttc915 <theomaniac917@gmail.com>
2025-07-27 19:51:40 +0300

    + 5

---
+++ contracts/AddFiveStorage.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24; // stating o ur vesion
+
+import {SimpleStorage} from "./SimpleStorage.sol";
+
+contract AddFiveStorage is SimpleStorage{
+    // + 5
+    // override
+    // virtual override
+    function store(uint256 _number) public override {
+
+    }
+    
+}
