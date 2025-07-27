c94cec78275065e7e1f3506129c2a0d90c134e18
ttc915 <theomaniac917@gmail.com>
2025-07-27 14:35:41 +0300

    updates

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,2 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19; // stating our vesion
+
+contract SimpleStorage {
+    // Basic types: boolen, uint, int, address, bytes
+    // bool hasFavoriteNumber = true;
+    uint256 myFavoriteNumber; // 0
+
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    // dynamic array
+    Person[] public listOfPeople; // []
+
+    // Person public pat = Person(6, "Pat");
+    // Person public mariah = Person({favoriteNumber : 7, name : "Mariah"});
+
+
+    function store(uint256 _favorite_number ) public {
+        myFavoriteNumber = _favorite_number;
+
+    }
+
+    // view, pure
+    function retrive() public view returns (uint256) {
+        return myFavoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfPeople.push( Person( _favoriteNumber, _name));
+    }
+}
